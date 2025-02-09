af28785c44ba2e090cef57014be1ecee52bef65c
Aksoni07 <ashishsoni7195@gmail.com>
2025-02-09 18:18:03 +0530

    check

--- CodeNova-server/utils/sendEmail.js
+++ CodeNova-server/utils/sendEmail.js
@@ -1,26 +1,26 @@
 const nodemailer = require('nodemailer');
 
-module.exports = async (email, subject, text) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      host: process.env.HOST, // smtp.gmail.com
-      service: process.env.SERVICE, //gmail
-      port: Number(process.env.EMAIL_PORT), //465 , ssl , tls
-      secure: Boolean(process.env.SECURE), //true
-      auth: {
-        user: process.env.USER, //our email at gmail.com
-        pass: process.env.PASS, //our emailpassword
-      },
-    });
-    await transporter.sendMail({
-      from: process.env.USER,
-      to: email,
-      subject: subject,
-      text: text,
-    });
-    console.log('email send successfully');
-  } catch (err) {
-    console.log('email not send');
-    console.error(err);
-  }
+module.exports = async(email, subject, text) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            host: process.env.HOST,
+            service: process.env.SERVICE,
+            port: Number(process.env.EMAIL_PORT),
+            secure: Boolean(process.env.SECURE),
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS,
+            },
+        });
+        await transporter.sendMail({
+            from: process.env.USER,
+            to: email,
+            subject: subject,
+            text: text,
+        });
+        console.log('email send successfully');
+    } catch (err) {
+        console.log('email not send');
+        console.error(err);
+    }
 };
